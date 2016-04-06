316cf5b95763a1953d855c215f1ff0ed70b1a8a6
Serghei Lungu <serghei.lungu@xing.com>
2016-04-06 21:23:43 +0200

    Initial Commit

---
+++ SamplePickerCalabashDebugConfig/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SamplePickerCalabashDebugConfig
+//
+//  Created by Serghei Lungu on 06/04/16.
+//  Copyright © 2016 new. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
